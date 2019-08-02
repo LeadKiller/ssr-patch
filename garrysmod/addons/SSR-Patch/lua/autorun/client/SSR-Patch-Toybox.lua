065f12d0e77d8611d2ec231ad1bd5f2615368354
LeadKiller <hl2epicfan@gmail.com>
2019-08-02 00:53:59 -0700

    v1.1 - Toybox Update
    Hide the old Toybox Entities, we're finished with things now.

--- garrysmod/addons/SSR-Patch/lua/autorun/client/SSR-Patch-Toybox.lua
+++ garrysmod/addons/SSR-Patch/lua/autorun/client/SSR-Patch-Toybox.lua
@@ -1,29 +1,29 @@
 if !SinglePlayer() then return end
 
 hook.Add("InitPostEntity", "SSR-Toybox", function()
-	if spawnmenu.GetCreationTabs()['Toybox'] and spawnmenu.GetCreationTabs()["Toybox Entities"] then
-		spawnmenu.GetCreationTabs()['Toybox'] = nil
-		--spawnmenu.GetCreationTabs()["Toybox Entities"] = nil
+	if spawnmenu.GetCreationTabs()["Toybox"] and spawnmenu.GetCreationTabs()["Toybox Entities"] then
+		spawnmenu.GetCreationTabs()["Toybox"] = nil
+		spawnmenu.GetCreationTabs()["Toybox Entities"] = nil
 
 		timer.Simple(1, function()
 			local PANEL = {}
 
 			function PANEL:Init()
-				self.HTMLControls = vgui.Create( "DHTMLControls", self ); 
+				self.HTMLControls = vgui.Create( "DHTMLControls", self );
 				self.HTMLControls:Dock( TOP )
 			end
 
 			function PANEL:Paint()
 				if ( !self.Started ) then
-		
+
 					self.Started = true;
-		
+
 					self.HTML = vgui.Create( "HTML", self )
 					self.HTML:Dock( FILL )
 					self.HTML:OpenURL( "http://toybox.garrysmod12.com/ingame/" );
-		
+
 					self.HTMLControls:SetHTML( self.HTML )
-		
+
 					local OldFunc = self.HTML.OpeningURL
 					self.HTML.OpeningURL = function( panel, url, target, postdata, bredirect )
 						if string.find(url, "lua") then
